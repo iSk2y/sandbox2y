5110bb461c88461214465c1bbc1d7a002bf1f059
iSk2y <isk2yroot@gmail.com>
2018-11-11 14:01:37 +0800

    组织架构列表展示，对应07节，使用datatables

--- apps/system/urls.py
+++ apps/system/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('', SystemView.as_view(), name='login'),
     path('basic/structure/', views_structure.StructureView.as_view(), name='basic_structure'),
     path('basic/structure/create/', views_structure.StructureCreateView.as_view(), name='basic_structure_create'),
+    path('basic/structure/list/', views_structure.StructureListView.as_view(), name='basic_structure_list'),
 ]
